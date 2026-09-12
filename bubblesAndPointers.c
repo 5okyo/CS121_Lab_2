34235f4e54f8e1d741a437bbd1000e48138d2471
5okyo <yoboime100@gmail.com>
2026-09-11 23:45:16 -0400

    fixed

--- bubblesAndPointers.c
+++ bubblesAndPointers.c
@@ -1,54 +1,56 @@
 #include <stdio.h>
-const int MAX=9;
+
+const int MAX = 9;
 
 void printValues(int*);
 void sort(int*);
 void swap(int*, int*);
 
+int main() {
+    int values[] = {7, 3, 9, 4, 6, 1, 2, 8, 5};
 
+    printf("Before: \n");
+    printValues(values);
 
-int main(){
-  int values[] = {7, 3, 9, 4, 6, 1, 2, 8, 5};
-  printf("Before: \n");
-  printValues(values);
+    // test swap
+    int x = 3;
+    int y = 5;
 
-  // test swap
-  int x = 3;
-  int y = 5;
-  printf("x: %d, y: %d \n", x, y);
-  swap(&x, &y);
-  printf("x: %d, y: %d \n", x, y);
+    printf("x: %d, y: %d \n", x, y);
+    swap(&x, &y);
+    printf("x: %d, y: %d \n", x, y);
 
-  sort(values);
-  printf("After: \n");
-  printValues(values);
+    sort(values);
 
-  return(0);
+    printf("After: \n");
+    printValues(values);
+
+    return 0;
 }
 
-
 void printValues(int* array) {
-	for (int i = 0; i < MAX; i++) {
-		printf("%d ", *(array + i));
-	}	
-	printf("\n");
-}	
+    for (int i = 0; i < MAX; i++) {
+        printf("%d ", *(array + i));
+    }
 
+    printf("\n");
+}
 
 void sort(int* array) {
-	int i, j; 
-	for (i = 0; i < MAX - 1, i++;) {
-		for (j = 0; j < MAX - 1; j++) {
-			if (*(array + j) > *(array + j + 1)) {
-				swap((array + j), (array + j + 1));
-				printValues(array);
-			}
-		}
-	}
+    int i, j;
+
+    for (i = 0; i < MAX - 1; i++) {
+        for (j = 0; j < MAX - 1; j++) {
+            if (*(array + j) > *(array + j + 1)) {
+                swap((array + j), (array + j + 1));
+                printValues(array);
+            }
+        }
+    }
 }
 
 void swap(int* x, int* y) {
-	int temp = *x;
-	*x = *y;
-	*y = temp; 	
-}	
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
